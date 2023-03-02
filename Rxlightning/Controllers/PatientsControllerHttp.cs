4382e1a2e836d14a3c321c5a2e73a365f121b3c7
Sebastian <sebasuchiha29.sg@gmail.com>
2023-03-02 08:28:57 -0500

    add http but pending jwt local

---
+++ Rxlightning/Controllers/PatientsControllerHttp.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Rxlightning.Interface;
+using Rxlightning.WebApi.Models;
+
+namespace Onetree.RxLightning.SeniorTechInterview.Al3xCubillos.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("patients")]
+    [Produces("application/json")]
+    public class PatientsController : ControllerBase
+    {
+        private readonly IPatientsHttp _patientService;
+        private readonly ILogger<PatientsController> _logger;
+
+        public PatientsController(IPatientsHttp patientService, ILogger<PatientsController> logger)
+        {
+            _patientService = patientService;
+            _logger = logger;
+        }
+
+        [HttpGet("")]
+        public async Task<IEnumerable<Patient>> Get()
+        {
+            var patients = await _patientService.GetAllAsync().ConfigureAwait(false);
+
+            return patients;
+        }
+
+        [HttpGet("/patient/{patientId}")]
+        [ProducesResponseType(typeof(Patient), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAsync(string patientId)
+        {
+            var patient = await _patientService.GetByIdAsync(patientId).ConfigureAwait(false);
+
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(patient);
+        }
+    }
+}
--- Rxlightning/Models/Patient.cs
+++ Rxlightning/Models/Patient.cs
@@ -2,11 +2,11 @@
 {
     public class Patient
     {
-        public Guid patientId { get; set; }
+        public string patientId { get; set; }
         public string firstName { get; set; }
         public string lastName { get; set; }
         public string gender { get; set; }
-        public DateTime dateOfBirth { get; set; }
+        public string dateOfBirth { get; set; }
         public string addressLine1 { get; set; }
         public string addressLine2 { get; set; }
         public string city { get; set; }
